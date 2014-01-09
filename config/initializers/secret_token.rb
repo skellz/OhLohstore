38eeb4284c622c9498861a4b62427efca3852475
skellz <sherwin.kelly@gmail.com>
2014-01-09 14:35:24 -0500

    Added OhLoh login authentication with OAuth

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Product::Application.config.secret_key_base = 'e57bfda1315d79d91cd4b6dfd88d5fecb45b0dee9e9fedd6021f5a3ad93e2ecbd3426e932443a308f3b1ca82bbd503080a83b96fd4d761a70a2431300c71e32e'
