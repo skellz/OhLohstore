ab40324cad6949505cdf14695d1ccf298eb384e0
skellz <skellz@outlook.com>
2014-01-10 09:03:37 -0500

    Update secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Product::Application.config.secret_key_base = 'e57bfda1315d79d91cd4b6dfd88d5fecb45b0dee9e9fedd6021f5a3ad93e2ecbd3426e932443a308f3b1ca82bbd503080a83b96fd4d761a70a2431300c71e32e'
+Product::Application.config.secret_key_base = ENV['SECRET_KEY']
